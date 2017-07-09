9f34ac28506e48c1ef99bf9448e26f1dbf613681
Kelly Hondula <khondula@rstudio03.research.sesync.org>
2017-07-08 20:12:18 -0400

    added calculating area of ag land

--- agbuffer.R
+++ agbuffer.R
@@ -1,6 +1,12 @@
 # crop somerset county land use data to ag
 # and add a 200m buffer
 
+library(raster)
+library(leaflet)
+library(rgdal)
+library(rgeos)
+library(RStoolbox)
+
 # use to download imagery data
 
 shp <- "../data/Some_2010LULC/"
@@ -18,10 +24,25 @@ plot(lulc_AG)
 crs(lulc_AG)
 
 ag200 <- gBuffer(lulc_AG, width=200)
-ag200 <- SpatialPolygonsDataFrame(ag200, as.data.frame(ag200))
 ag200 <- as(ag200, "SpatialPolygonsDataFrame")
-writeOGR(ag200, dsn = ".", "ag200", driver = "ESRI Shapefile")
+
+# writeOGR(ag200, dsn = ".", "ag200", driver = "ESRI Shapefile")
 
 # also write to google earth
 ag200GE <- spTransform(ag200, CRS("+proj=longlat +datum=WGS84"))
-writeOGR(ag200GE, "ag200.kml", "ag200", "KML")
+# writeOGR(ag200GE, "ag200.kml", "ag200", "KML")
+
+# calculate areas 
+
+# just ag land
+# units is m
+sum(area(lulc_AG))/10000 # hecatres
+sum(area(lulc_AG))/1e+6 # sq km
+sum(area(lulc_AG))/4046.86 # acres
+# MD planning website 
+# https://planning.maryland.gov/PDF/OurWork/LandUse/County/Somerset.pdf
+# ag land use in 2010 is listed as 49,693 acres
+
+# ag land buffered to 200m
+sum(area(ag200))/4046.86 # acres
+# buffered area is over twice that size at 112,229.2 acres
